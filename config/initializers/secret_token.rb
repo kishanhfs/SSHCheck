b4c4faa21094cefd1dc2e25d1919fa9324483ef4
Kishan Borkhatariya <kishan@headfitted.com>
2011-07-16 20:17:46 +0530

    First commit for check

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SSHCheck::Application.config.secret_token = '43164e98205fd9ce06b43b88dff6ae96d5ee23cfc9b700eba213b5df4f5ef30e2598a89dd3e1f1ed3feed4f35804051be57d7194e534bcbcff800c8d1eb02df3'
